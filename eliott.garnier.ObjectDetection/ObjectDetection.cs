e096c266aedd9c6853ec4a2bf7fb957bd065a147
Kirite <eliott.garnier2810@gmail.com>
2025-01-14 08:24:27 +0100

    Initiation de la solution (.sln) et création du projet librairie

---
+++ eliott.garnier.ObjectDetection/ObjectDetection.cs
@@ -0,0 +1,14 @@
+﻿namespace eliott.garnier.ObjectDetection;
+
+public class ObjectDetection
+{
+
+    public async Task<IList<ObjectDetectionResult>>
+        DetectObjectInScenesAsync(IList<byte[]> imagesSceneData)
+    {
+        await Task.Delay(1000);
+// TODO implement your code here
+        throw new NotImplementedException();
+    }
+
+}
