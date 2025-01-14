de87681ab46f6457f48b206422e9b74ef98102d2
Kirite <eliott.garnier2810@gmail.com>
2025-01-14 08:48:16 +0100

    Création d’un projet de test unitaire

--- eliott.garnier.ObjectDetection/ObjectDetectionResult.cs
+++ eliott.garnier.ObjectDetection/ObjectDetectionResult.cs
@@ -1,7 +1,24 @@
-﻿namespace eliott.garnier.ObjectDetection;
+﻿using Microsoft.ML;
+
+namespace eliott.garnier.ObjectDetection;
 
 public record ObjectDetectionResult
 {
     public byte[] ImageData { get; set; }
     public IList<BoundingBox> Box { get; set; } 
 } 
+
+public class Dimensions
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Height { get; set; }
+    public float Width { get; set; }
+}
+
+public class BoundingBox
+{
+    public Dimensions Dimensions { get; set; }
+    public string Label { get; set; }
+    public float Confidence { get; set; }
+}
